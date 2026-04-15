5103e21756d2d2c6242ba7d21b1086c077596fcf
parinitha768 <parinitha299@gmail.com>
2026-04-15 15:34:04 +0530

    Update App.py

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ def load_summarizer():
   summarizer = load_summarizer()
 
 #streamlit UI
-st.title("EVO AI Text Summarizer")
+st.title("AI Text Summarizer")
 st.write("Enter a long text below, and get a concise summary!")
 #Text Input
 long_text = st.text_area("Enter text to Summarize:", height=200)
