8a71297c78aa6f2e794653025f8ca0234d8632e2
parinitha768 <parinitha299@gmail.com>
2026-04-15 15:41:13 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 
 @st.cache_resource
 def load_summarizer():
-  return pipeline("summarization", model="google/gemma-3")
+  return pipeline("summarization", model=""sshleifer/distilbart-cnn-12-6")
   summarizer = load_summarizer()
 
 #streamlit UI
