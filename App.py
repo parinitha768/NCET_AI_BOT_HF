0e2bd1d2e43b08300af9cdf244ae693696c6e223
parinitha768 <parinitha299@gmail.com>
2026-04-15 14:51:12 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from transformers import pipeline
 def load_summarizer():
-  return pipline("Summarization", model="sshleifer/distibart-cnn-12-6")
+  return pipline("Summarization", model="sshleifer/distilbart-cnn-12-6")
 summarizer=load_summrizer()
 st.title("AI Text Summarizer")
 st.write("Enter a long text below, and get a concise summary!")
 long_text=st.text_area("Enter text to summarize:",height=200)
 max_length=st.slider("Max Summary Length",min_value=50,
-                     max_value=300, value=130")
+                     max_value=300,value=130")
 min_length=st.slider("Min Summary Length", min_value=20,
-                     max_value=100, value=30")
+                     max_value=100,value=30")
 if st.button("Summarizer"):
   if long_text.strip():
     with st.spinner("Generating summary..."):
